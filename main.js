2cd34e5979ec5b858a052259c03e8d8149a47697
ilkhom <rilhom21@gmail.com>
2023-02-10 16:59:21 +0500

    all

--- main.js
+++ main.js
@@ -18,8 +18,29 @@ btns?.addEventListener("click", () => {
   hamburgers.classList.toggle("show");
   menus.classList.toggle("show");
 });
-const menu = document.querySelectorAll(".r_top");
-const desc = document.querySelectorAll(".serv_desc");
+
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
 
 // menu?.addEventListener("click", () => {
 //   desc.classList.toggle("show");
